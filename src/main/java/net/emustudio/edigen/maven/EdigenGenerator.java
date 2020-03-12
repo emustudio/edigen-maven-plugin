091b765214c77e20db54fa94d0b64310479ef13e
Peter Jakubčo <pjakubco@gmail.com>
2020-03-12 07:55:23 +0100

    Fixed some javadoc + bumped maven plugin versions

--- src/main/java/net/emustudio/edigen/maven/EdigenGenerator.java
+++ src/main/java/net/emustudio/edigen/maven/EdigenGenerator.java
@@ -20,128 +20,120 @@ package net.emustudio.edigen.maven;
 import net.emustudio.edigen.Edigen;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.project.MavenProject;
+import org.apache.maven.plugins.annotations.Mojo;
 
 import java.io.File;
 
 /**
  * Edigen Maven plugin.
+ *
  * @author Matúš Sulír
- * 
- * @goal generate
- * @phase generate-sources
  */
+@SuppressWarnings("unused")
+@Mojo(name = "generate", defaultPhase = LifecyclePhase.GENERATE_SOURCES)
 public class EdigenGenerator extends AbstractMojo {
 
     /**
      * An object representing the Maven project which uses this plugin in its
      * POM file.
-     * 
-     * @parameter property="project"
-     * @required
-     * @readonly
      */
+    @Parameter(property = "project", required = true, readonly = true)
     private MavenProject project;
-    
+
     /**
      * Specification file for Edigen.
-     * 
-     * @parameter default-value="${basedir}/src/main/edigen/cpu.eds"
      */
+    @Parameter(defaultValue = "${basedir}/src/main/edigen/cpu.eds")
     private File specification;
-    
+
     /**
      * Instruction decoder package + class name.
-     * 
-     * @parameter
-     * @required
      */
+    @Parameter(required = true)
     private String decoderName;
-    
+
     /**
      * Disassembler package + class name.
-     * 
-     * @parameter
-     * @required
      */
+    @Parameter(required = true)
     private String disassemblerName;
-    
+
     /**
      * Disassembler output directory.
-     * 
+     * <p>
      * The default is target/generated-sources/edigen/ + path obtained from
      * the package name.
-     * 
-     * @parameter
      */
+    @Parameter
     private File disassemblerOutputDir;
-    
+
     /**
      * Disassembler template file.
-     * 
-     * @parameter
      */
+    @Parameter
     private File disassemblerTemplate;
-    
+
     /**
      * Debug mode (display tree transformations).
-     * 
-     * @parameter
      */
+    @Parameter
     private boolean debug;
-    
+
     /**
      * Instruction decoder output directory.
-     * 
+     * <p>
      * The default is target/generated-sources/edigen/ + path obtained from
      * the package name.
-     * 
-     * @parameter
-     */    
+     */
+    @Parameter
     private File decoderOutputDir;
-    
+
     /**
      * Instruction decoder template file.
-     * 
-     * @parameter
      */
+    @Parameter
     private File decoderTemplate;
-    
+
     /**
      * A list of command-line arguments passed to Edigen.
      */
     private ArgumentList arguments;
-    
+
     /**
      * Executes the goal "generate" of this plugin.
+     *
      * @throws MojoExecutionException when the goal execution is unsuccessful
      */
     @Override
     public void execute() throws MojoExecutionException {
         validate();
-        
+
         arguments = new ArgumentList(project);
         addArguments();
-        
+
         try {
             new Edigen().run(arguments.get());
         } catch (Exception ex) {
             throw new MojoExecutionException(ex.getMessage(), ex);
         }
     }
-    
+
     /**
      * Validates the project configuration.
+     *
      * @throws MojoExecutionException when the configuration is invalid
      */
     private void validate() throws MojoExecutionException {
         if (!decoderName.contains("."))
             throw new MojoExecutionException("Decoder name must include a package.");
-        
+
         if (!disassemblerName.contains("."))
             throw new MojoExecutionException("Disassembler name must include a package.");
     }
-    
+
     /**
      * Adds command-line arguments to the list, according to the configuration.
      */
@@ -149,14 +141,13 @@ public class EdigenGenerator extends AbstractMojo {
         arguments.add(specification.getPath());
         arguments.add(decoderName);
         arguments.add(disassemblerName);
-        
+
         arguments.addOutputDirectory("-ao", disassemblerOutputDir, disassemblerName);
         arguments.addTemplate("-at", disassemblerTemplate);
-        
+
         arguments.addFlag("-d", debug);
-        
+
         arguments.addOutputDirectory("-do", decoderOutputDir, decoderName);
         arguments.addTemplate("-dt", decoderTemplate);
     }
-    
 }
